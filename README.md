3ed7262537653114264b5c3ef4f25d3bbb5f57f7
Stéphane <stephane.tiv@gmail.com>
2018-03-13 14:24:25 +0100

    Spark TP1 : température max par mois sur les 2 années 1901 et 1902

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # TpSpark1
+
+## Statistiques sur des données météo avec l'API RDDs
+
+Température max par mois sur les deux années 1901 et 1902
---
+++ sparkMeteo.py
@@ -0,0 +1,21 @@
+from pyspark import SparkContext
+import sys
+
+#initialize config
+sc = SparkContext(master='local[2]', appName='SparkMeteo')
+
+#read file from stdin directory/file
+lines = sc.textFile(sys.argv[1])
+# map : get date, temperature, quality
+# filter : bad temperature and bad quality
+# map : get month, temperature as Celsius
+# reduce : reduce by month
+# sort : sort by mont
+values = lines.map(lambda ligne: (ligne[15:24], ligne[87:92], ligne[92]))\
+.filter(lambda (date, temp, quality): int(temp)!=9999 and int(quality) in (0,1,4,5,9))\
+.map(lambda (date, temp, quality):((str(date))[4:6], float(temp)/10))\
+.reduceByKey(max)\
+.sortByKey()
+# write max temp per month in stdout file
+values.saveAsTextFile(sys.argv[2])
+	
